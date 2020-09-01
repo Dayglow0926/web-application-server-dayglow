bab17cb121ed895c7020bdc045779cac754a9bb0
JooKwang <dayglow0926@gmail.com>
2020-09-02 00:22:14 +0900

    로그인 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,23 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import db.DataBase;
+import model.User;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,14 +33,91 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다. 
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        	String line = br.readLine();
+        	log.debug("request line : {}", line);
+        	
+        	if(line == null) {
+        		return ;
+        	}
+        	
+        	String[] tokens = line.split(" ");
+        	int contentLength = 0;
+        	
+        	while(!line.equals("")) {
+        		log.debug("header : {}",line);
+        		line = br.readLine();
+        		if(line.contains("Content-Length")) {
+        			contentLength = getContentLength(line);
+        		}
+        	}
+        	
+        	String url = tokens[1];
+        	
+        	if("/user/create".equals(url)) {
+        		//int index = url.indexOf("?");
+        		//String queryString = url.substring(index+1);
+        		
+        		String body = IOUtils.readData(br, contentLength);
+        		Map<String, String> params = HttpRequestUtils.parseQueryString(body);
+        		
+        		User user = new User(params.get("name"), params.get("email"), params.get("name"),params.get("email"));
+        		
+        		DataBase.addUser(user);
+        		
+        		log.debug("User : {}",user);
+        		DataOutputStream dos = new DataOutputStream(out);
+        		response302Header(dos,"/index.html");
+        		
+        		//url = "/index.html";
+        	}else if("/user/login".equals(url)) {
+        		String body = IOUtils.readData(br, contentLength);
+        		Map<String, String> params = HttpRequestUtils.parseQueryString(body);
+        		User user = DataBase.findUserById(params.get("userId"));
+        		if(user == null) {
+        			responseResource(out, "/user/login_failed.html");
+        			
+        		}
+        	}
+        	
+        	movePage(url,out);
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+    
+    private void responseResource(OutputStream out, String url) throws IOException {
+		DataOutputStream dos = new DataOutputStream(out);
+		byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+		response200Header(dos, body.length);
+		responseBody(dos, body);
+		
+	}
+
+	private void response302Header(DataOutputStream dos, String url) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+			dos.writeBytes("Location:"+url+"\r\n");
+			dos.writeBytes("\r\n");
+		}catch(IOException e) {
+			log.error(e.getMessage());
+		}
+		
+	}
+
+	private int getContentLength(String line) {
+    	String[] headerTokens = line.split(":");
+    	return Integer.parseInt(headerTokens[1].trim());
+    }
+    
+    private void movePage(String url, OutputStream out) throws IOException{
+    	DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+        
+        response200Header(dos, body.length);
+        responseBody(dos, body);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
