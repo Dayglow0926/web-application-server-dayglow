e082a74a6182afd6244605f6f695f22e84bf85d3
JooKwang <dayglow0926@gmail.com>
2020-09-02 00:39:57 +0900

    로그인 구현 재 업로드

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -33,7 +33,6 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-<<<<<<< HEAD
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다. 
         	BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
         	String line = br.readLine();
@@ -84,13 +83,6 @@ public class RequestHandler extends Thread {
         	
         	movePage(url,out);
 
-=======
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello Gun".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
->>>>>>> refs/remotes/origin/master
         } catch (IOException e) {
             log.error(e.getMessage());
         }
